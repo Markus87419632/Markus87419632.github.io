336f70842816f42c388a1d1bcfbfbdadc90c74b4
Markus <markus87419632@gmail.com>
2025-10-23 17:53:19 +0800

    app

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'personal', 'awards','life','personalPRO','awardsPRO','lifePRO']
+const section_names = ['home', 'personal', 'awards','life','personalPRO','awardsPRO','lifePRO','app']
 
 
 window.addEventListener('DOMContentLoaded', event => {
